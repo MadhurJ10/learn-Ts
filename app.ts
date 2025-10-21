29c8373fe299154d994c06ea85dc82811b0f1620
Madhur Bhawsar <madhurbhawsar7@gmail.com>
2025-10-21 11:53:49 +0530

    interfaces , type aliases

--- app.ts
+++ app.ts
@@ -1,10 +1,10 @@
 //tuples
 
-let arr: [number, string] = [20 , "madhur"]
+let arr: [number, string] = [20, "madhur"]
 
 
 // enum
-enum human{
+enum human {
     canFly = "false",
     canTalk = "true"
 }
@@ -13,4 +13,38 @@ console.log(human.canFly)
 console.log(human.canTalk)
 
 // any , unknown, void , null
-let a: string ='';
+let a: string = '';
+
+
+//interfaces 
+interface user {
+    name: string,
+    email: string,
+    num: number
+}
+
+function getUserdet(obj: user) {
+
+}
+
+getUserdet({ name: "madhur", email: "madhur", num: 20 });
+
+//interface extensions 
+
+interface admin extends user {
+    isAdmin: boolean
+}
+
+interface superAdmin extends admin { 
+    isSuperadmin: boolean;
+}
+function check(obj: superAdmin) {
+    obj.isSuperadmin = true;
+}
+
+// type aliases
+
+type value = string | number;
+
+let val:value = 8;
+val = 'asn';  
